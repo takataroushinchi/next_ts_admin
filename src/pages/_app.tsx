17364cd828494f39753ca4c530e8d3c5709f8e33
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2022-06-10 18:58:13 +0900

    MantineとTailwind CSSが競合してしまう問題対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,27 @@
-import "src/lib/tailwind.css";
-import type { AppProps } from "next/app";
-import Link from "next/link";
-import { MantineProvider } from "@mantine/core";
+import 'src/lib/tailwind.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      emotionOptions={{ key: 'mantine', prepend: false }}
+    >
       <div className="mx-auto max-w-prose">
         <header>
           <Link href="/">
-            <a className="text-xl font-bold py-8">タイトル</a>
+            <a className="py-8 text-xl font-bold">タイトル</a>
           </Link>
         </header>
-        <main className="text-gray-600 mt-8">
+        <main className="mt-8 text-gray-600">
           <Component {...pageProps} />
         </main>
       </div>
     </MantineProvider>
-  )
+  );
 }
 
 export default MyApp;
