cbc229ed2aa9415889cded7a2d1a6d61b8c55709
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2022-06-19 14:53:55 +0900

    タブスタイル変更

--- src/components/ui/Tab.tsx
+++ src/components/ui/Tab.tsx
@@ -7,12 +7,12 @@ type Props = {
 export const Tab: React.VFC<Props> = ({ current }) => {
   return current === 'top' ? (
     <nav className="flex gap-x-2 pb-8">
-      <a className="flex-1 space-y-4 rounded-sm border-b-2 border-slate-600 bg-white p-4">
-        <p className="text-sm font-semibold text-slate-600">トップ</p>
+      <a className="flex-1 space-y-4 rounded-sm border-b-2 border-slate-600 bg-sky-700 p-4">
+        <p className="text-sm font-semibold text-slate-200">トップ</p>
       </a>
       <Link href={`/list`}>
-        <a className="group flex-1 space-y-4 rounded-sm bg-sky-700 p-4 shadow ring-1 ring-slate-900/5 hover:bg-sky-600 hover:ring-sky-600">
-          <p className="text-sm font-semibold text-slate-200 group-hover:text-slate-100">
+        <a className="group flex-1 space-y-4 rounded-sm bg-slate-50 p-4 shadow ring-1 ring-slate-900/5 hover:bg-sky-600 hover:ring-sky-600">
+          <p className="group-hover:text-bg-sky-600 text-sm font-semibold text-sky-600 group-hover:text-slate-50">
             案件リスト
           </p>
         </a>
@@ -21,14 +21,14 @@ export const Tab: React.VFC<Props> = ({ current }) => {
   ) : (
     <nav className="flex gap-x-2 pb-8">
       <Link href={`/`}>
-        <a className="group flex-1 space-y-4 rounded-sm bg-sky-700 p-4 shadow ring-1 ring-slate-900/5 hover:bg-sky-600 hover:ring-sky-600">
-          <p className="text-sm font-semibold text-slate-200 group-hover:text-slate-100">
+        <a className="group flex-1 space-y-4 rounded-sm bg-slate-50 p-4 shadow ring-1 ring-slate-900/5 hover:bg-sky-600 hover:ring-sky-600">
+          <p className="group-hover:text-bg-sky-600 text-sm font-semibold text-sky-600 group-hover:text-slate-50">
             トップ
           </p>
         </a>
       </Link>
-      <a className="flex-1 space-y-4 rounded-sm border-b-2 border-slate-600 bg-white p-4">
-        <p className="text-sm font-semibold text-slate-600">案件リスト</p>
+      <a className="flex-1 space-y-4 rounded-sm border-b-2 border-slate-600 bg-sky-700 p-4">
+        <p className="text-sm font-semibold text-slate-200">案件リスト</p>
       </a>
     </nav>
   );
