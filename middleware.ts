f1466c2017434ba76aee4551d64aa6f201afc4cd
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2022-07-08 11:52:58 +0900

    use middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function middleware (request: NextRequest) {
-  if (request.nextUrl.pathname === '/test')
-    const url! = request.nextUrl.clone()
-    url.pathname = '/layout-test'
-    return NextResponse.redirect(url)
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/test') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/layout-test';
+    return NextResponse.redirect(url);
   }
 }
 
 export const config = {
-  matcher: ['/test']
-}
+  matcher: ['/test'],
+};
